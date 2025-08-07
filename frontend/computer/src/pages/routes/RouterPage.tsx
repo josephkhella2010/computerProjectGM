74ecc3808f14b7f0700164bdbe9ff9c1bc8fc3ed
Joseph Khella <josephkhella2010@gmail.com>
2025-08-07 16:30:22 +0200

    add hash routes

--- frontend/computer/src/pages/routes/RouterPage.tsx
+++ frontend/computer/src/pages/routes/RouterPage.tsx
@@ -1,4 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+/* import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeSection from "../HomePage/HomeSection";
+import NavigationSection from "../Navigation/NavigationSection";
+import ContactUsPage from "../ContactUs/ContactUsPage";
+import AboutUsPage from "../aboutUs/AboutUsPage";
+import Footer from "../footer/Footer";
+import ProductSection from "../../ProductPage/ProductSection";
+import SingleProductSection from "../singlePorductPage/SingleProductSection";
+
+export default function RouterPage() {
+  return (
+    <Router>
+      <NavigationSection />
+      <Routes>
+        <Route path="/" element={<HomeSection />} />
+        <Route path="/contactUs" element={<ContactUsPage />} />
+        <Route path="/aboutUs" element={<AboutUsPage />} />
+        <Route path="/product" element={<ProductSection />} />
+        <Route path="/product/:id" element={<SingleProductSection />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+ */
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomeSection from "../HomePage/HomeSection";
 import NavigationSection from "../Navigation/NavigationSection";
 import ContactUsPage from "../ContactUs/ContactUsPage";
