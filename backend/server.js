f3f7efbbbeda14e27354bf07b24439ab76039f96
joseph khella <josephkhella2010@gmail.com>
2026-05-22 16:07:31 +0200

    fix error two

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -5,11 +6,11 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-/* const getProducts = require("./ApiRoutes/ProductRoutes");
+ const getProducts = require("./ApiRoutes/ProductRoutes");
 app.use(getProducts); 
 app.use("/api", require("./ApiRoutes/ProductRoutSqlite"));
 app.get("/test", (req, res) => res.send("Server is running!"));
-*/
+
 app.use("/api/send-email", require("./ApiRoutes/sendEmail"));
 
 const PORT = process.env.PORT || 3100;
@@ -19,3 +20,32 @@ app.listen(PORT, () =>
     `🚀 Server running on http://localhost:${process.env.PORT || 3100}`,
   ),
 );
+
+*/
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+  }),
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+app.use("/api/send-email", require("./ApiRoutes/sendEmail"));
+
+const PORT = process.env.PORT || 3100;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port http://localhost:${PORT}`);
+});
